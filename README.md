e2e57d9ae38ea712599e564b75690d2773832f2c
Anthony <anthony@anthonys-mbp.lan>
2018-10-05 20:56:41 -0400

    Added task list

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Task List
+1. Get data from the postgreSQL DB. Display 1yr YTD yield, and volatility on a daily basis
+2. Make scripts to get daily price quote and post to DB
+3. Make volatility calculation more accurate based on high/low
+4. Make volatility calculation more accurate based on open/mid/close
+5. Make volatility calculation more accurate based on hourly pricing
+6. Make volatility calculation more accurate based on minute pricing 
+7. Make volatility calculation more accurate based on seconds pricing 
+
 # Purpose
 Easily compare public stocks by the risk-adjusted-yield. 
 
---
+++ data.py
@@ -0,0 +1,40 @@
+def Companies():
+    companies = [
+        {
+            "id": 1,
+            "name": "Tesla",
+            "price2015": "2.2%",
+            "price2016": "1.2%",
+            "price2017": "17.2%",
+            "beta": "2",
+            "stockSymbol": "T"
+        },
+        {
+            "id": 2,
+            "name": "Google",
+            "price2015": "27.2%",
+            "price2016": "17.2%",
+            "price2017": "7.2%",
+            "beta": "1.79",
+            "stockSymbol": "GOOG"
+        },
+        {
+            "id": 3,
+            "name": "Genentech",
+            "price2015": "2.22%",
+            "price2016": "1.72%",
+            "price2017": ".72%",
+            "beta": "1.5",
+            "stockSymbol": "DNA"
+        },
+        {
+            "id": 4,
+            "name": "Apple",
+            "price2015": "2.2%",
+            "price2016": "1.2%",
+            "price2017": "1.7%",
+            "beta": "1.56",
+            "stockSymbol": "APPL"
+        }
+    ]
+    return companies
