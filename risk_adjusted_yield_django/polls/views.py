6582ca6d7f904d3bbe099f770b0490d73b0c0db1
Anthony <anthony@anthonys-mbp.lan>
2018-10-12 16:42:07 -0400

    Switch to Django

--- risk_adjusted_yield_django/polls/views.py
+++ risk_adjusted_yield_django/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -9,7 +9,9 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return render(request, "You're looking at question %s." % question_id, context)
+    # Takes a Django Model and keyword to pass into the get request
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
